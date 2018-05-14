1aff12f09a7a3ed13f9ebc0a4526f96c048e57d5
Даурен Иманбаев <Dauren.Imanbayev@kaspi.kz>
2018-05-14 17:55:51 +0600

    Cart-table

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -21,17 +21,16 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 from .views import home_page, about_page, contact_page, login_page, register_page
-from carts.views import cart_home
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^about/$', about_page, name='about'),
     url(r'^contact/$', contact_page, name='contact'),
     url(r'^login/$', login_page, name='login'),
-    url(r'^cart/$', cart_home, name='cart'),
     url(r'^register/$', register_page, name='register'),
-    url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^products/', include("products.urls", namespace='products')),
+    url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
+    url(r'^cart/', include("carts.urls", namespace='cart')),
     url(r'^search/', include("search.urls", namespace='search')),
     url(r'^admin/', admin.site.urls),
 ]
