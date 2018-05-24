83b4b47e4cc5c05319a6f554b64203c5780dba24
Dauren Imanbayev <daureni@gmail.com>
2018-05-24 22:06:03 +0600

    jQuery-Contact Form

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 
 from .forms import ContactForm
@@ -28,6 +28,15 @@ def contact_page(request):
 	}
 	if contact_form.is_valid():
 		print(contact_form.cleaned_data)
+		if request.is_ajax():
+			return JsonResponse({"message": "Thank you for your submission"})
+
+	if contact_form.errors:
+		errors = contact_form.errors.as_json()
+		if request.is_ajax():
+			return HttpResponse(errors, status=400, content_type='application/json')
+
+
 	# if request.method == "POST":	
 	# 	print(request.POST)
 	# 	print(request.POST.get('fullname'))
